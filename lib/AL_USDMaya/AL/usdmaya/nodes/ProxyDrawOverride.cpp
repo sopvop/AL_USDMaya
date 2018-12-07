1bcf4fa2031e8ffe24c4dd9444e186990156c0c0
Pal Mezei <sirpalee@gmail.com>
2018-12-07 14:36:27 +1100

    Updating the code to use the new classes, structs and enums of the dev branch.

--- lib/AL_USDMaya/AL/usdmaya/nodes/ProxyDrawOverride.cpp
+++ lib/AL_USDMaya/AL/usdmaya/nodes/ProxyDrawOverride.cpp
@@ -59,9 +59,9 @@ public:
   ~RenderUserData()
     {}
 
-  UsdImagingGLEngine::RenderParams m_params;
+  UsdImagingGLRenderParams m_params;
   UsdPrim m_rootPrim;
-  UsdImagingGLHdEngine* m_engine = 0;
+  UsdImagingGLEngine* m_engine = 0;
   ProxyShape* m_shape = 0;
   MDagPath m_objPath;
 };
@@ -399,8 +399,8 @@ void ProxyDrawOverride::draw(const MHWRender::MDrawContext& context, const MUser
     ptr->m_params.frame = ptr->m_shape->outTimePlug().asMTime().as(MTime::uiUnit());
     if(combined.size())
     {
-      UsdImagingGLEngine::RenderParams params = ptr->m_params;
-      params.drawMode = UsdImagingGLEngine::DRAW_WIREFRAME;
+      UsdImagingGLRenderParams params = ptr->m_params;
+      params.drawMode = UsdImagingGLDrawMode::DRAW_WIREFRAME;
       MColor colour = M3dView::leadColor();
       params.wireframeColor = GfVec4f(colour.r, colour.g, colour.b, 1.0f);
       glDepthFunc(GL_LEQUAL);
